e9d36da7d42abce1b3675389316450ca8b051d1d
s952163 <s995110@yahoo.co.jp>
2016-06-13 08:53:10 +0900

    Add project files.

---
+++ Gjallarhorn160611/Model.fs
@@ -0,0 +1,9 @@
+﻿module Model
+
+open OxyPlot
+
+let rnd = System.Random()   
+let makeSeries n =
+    seq {for _ in 1..n -> rnd.NextDouble() - 0.5} |> Seq.scan (+) 0. |> Seq.mapi (fun x y -> DataPoint(float x,y)) |> Seq.toList
+
+
---
+++ Gjallarhorn160611/Program.fs
@@ -0,0 +1,20 @@
+﻿open System
+open Gjallarhorn
+open Gjallarhorn.Bindable
+open Gjallarhorn.Validation
+open FsXaml
+open System.Windows
+
+
+type MainWin = XAML<"MainWindow2.xaml">
+
+[<STAThread>]
+[<EntryPoint>]
+let main _ =     
+
+    Gjallarhorn.Wpf.install true |> ignore
+
+    let app = Application()
+    let win = MainWin(DataContext = Context3.create())
+    app.Run win
+    
